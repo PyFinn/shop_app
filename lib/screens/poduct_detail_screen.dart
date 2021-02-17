86cdcbdffb2d86302b1e7ba5c2935be1021bb9b4
PyFinn <realstraved@gmail.com>
2021-02-17 16:18:22 +0100

    More Provider Listeners

--- lib/screens/poduct_detail_screen.dart
+++ lib/screens/poduct_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
 
@@ -8,10 +10,11 @@ class ProductDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
 
     final String productId = ModalRoute.of(context).settings.arguments as String;
+    final loadedProduct = Provider.of<Products>(context, listen: false).findById(productId);
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hello'),
+        title: Text(loadedProduct.title),
       ),
     );
   }
